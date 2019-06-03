6a64cfe5c409d07820c95ad5ff5acc4a40c33fd7
SamWolfen <27791395+SamWolfen@users.noreply.github.com>
2019-06-04 00:43:31 +1000

    CHanged town layout although town center is fkd

---
+++ assets/tiles/SugarSpiteAllTiles.tsx
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="SugarSpiteAllTiles" tilewidth="48" tileheight="16" tilecount="18" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="BrickTile.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="BlackTile.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="BlockSpotTile.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="BlockTile.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="DoorTile - Copy.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="DoorTile.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="FenceTile - Copy.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="GrassTile.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="LogsSlopedTileLeft.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="LogsSlopedTileRight.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="LogsTile.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="RockWallTile.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="SignTile - Copy.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="WaterTile.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="StatueTileBottom.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="StatueTiletop.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="FenceTile2.png"/>
+ </tile>
+ <tile id="23">
+  <image width="48" height="16" source="Sign.png"/>
+ </tile>
+</tileset>
